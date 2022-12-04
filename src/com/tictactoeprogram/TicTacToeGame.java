e64596dea1a59dc6124632463db53e906e9e3bd4
dhiru0904 <dhirajdevre4@gmail.com>
2022-12-04 21:33:04 +0530

    Added:toss to check who play first computer or player

--- src/com/tictactoeprogram/TicTacToeGame.java
+++ src/com/tictactoeprogram/TicTacToeGame.java
@@ -4,6 +4,11 @@ public class TicTacToeGame {
     /*
     create a board method
      */
+    public static final int HEAD = 0;
+    public static final int TAIL = 1;
+    public static enum Player {
+        player1, player2
+    };
     public static char[] createBoard() {
         char[] board = new char[10];
         for (int position = 1; position < 10; position++) {
@@ -14,20 +19,17 @@ public class TicTacToeGame {
     public static char getInput(char givenInput) {
         return Character.toUpperCase(givenInput);
     }
-    public static void showBoard(char[] board) {/*show board method*/
+    public static void showBoard(char[] board){/*create showboard method*/
         System.out.println(board[1] + " | " + board[2] + " |" + " " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " |" + " " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " |" + " " + board[9]);
     }
-    /*
-    check the position
-     */
     public static boolean isBoardEmpty(char[] board, int index) {
         return board[index] == ' ';
     }
-    public static int getUserMove(char[] board, Scanner getInput) { /*get user move method*/
+    public static int getUserMove(char[] board, Scanner getInput) {
         while (true) {
             System.out.println("Enter the index you want to move");
             int index = getInput.nextInt();
@@ -48,6 +50,16 @@ public class TicTacToeGame {
             board[index] = letterInput;
         }
     }
+    private static Player tossWhoStartsFirst() {
+        int tossResult = (int) (Math.floor(Math.random() * 10)) % 2;
+        if (tossResult == HEAD) {
+            System.out.println("player will start");
+            return Player.player1;
+        } else {
+            System.out.println("computer will start");
+            return Player.player2;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game Program");
         Scanner userInput = new Scanner(System.in);
@@ -59,5 +71,6 @@ public class TicTacToeGame {
         showBoard(board);
         makeMove(board, userMove, getLetterInput);
         showBoard(board);
+        Player player = tossWhoStartsFirst();
     }
 }
